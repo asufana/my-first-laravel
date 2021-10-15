f10ebde44e0582b52e23ce242961c75bc976bbda
asufana <asufanah@gmail.com>
2021-10-15 18:18:17 +0900

    不要ライブラリ削除、CI調整ほか

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,8 +7,7 @@ $finder = PhpCsFixer\Finder::create()
     ->in([
         __DIR__ . '/app',
         __DIR__ . '/config',
-        __DIR__ . '/database/factories',
-        __DIR__ . '/database/seeders',
+        __DIR__ . '/database',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
     ]);
